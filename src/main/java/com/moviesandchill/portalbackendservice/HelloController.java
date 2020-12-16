0c4f6da8240fa935df7c037ff68eca7bb10e3eba
AnnaAlexandrovna <rassvetnavolge1>
2020-12-16 23:02:17 +0300

    moveeeee

--- src/main/java/com/moviesandchill/portalbackendservice/HelloController.java
+++ src/main/java/com/moviesandchill/portalbackendservice/HelloController.java
@@ -1,4 +1,4 @@
-package springboot;
+package com.moviesandchill.portalbackendservice;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
