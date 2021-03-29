e0cf559d1c76da42363cc6fa44c24b3a9cb535bd
weldnor <kolesov.anton.s@gmail.com>
2021-03-29 11:07:51 +0300

    Change allowedOrigins

--- src/main/java/com/moviesandchill/portalbackendservice/config/CorsConfig.java
+++ src/main/java/com/moviesandchill/portalbackendservice/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                 .allowCredentials(true);
     }
