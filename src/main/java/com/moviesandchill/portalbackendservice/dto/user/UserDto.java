be69a26394fc2927a8184a1a756279ccbf19e5b1
weldnor <kolesov.anton.s@gmail.com>
2021-04-07 23:51:25 +0300

    Update user dtos

--- src/main/java/com/moviesandchill/portalbackendservice/dto/user/UserDto.java
+++ src/main/java/com/moviesandchill/portalbackendservice/dto/user/UserDto.java
@@ -19,10 +19,6 @@ public class UserDto implements Serializable {
 
     private String email;
 
-    private String firstName;
-
-    private String lastName;
-
     private LocalDate birthday;
 
     private String logoUrl;
