da42802f39ee9e08093a6a59eb5d02bfce75c96d
NartovEvgenii <evgeniynartov36rus@gmail.com>
2021-04-25 15:54:09 +0300

    Update FilmServiceImpl.java

--- src/main/java/com/moviesandchill/portalbackendservice/service/film/impl/FilmServiceImpl.java
+++ src/main/java/com/moviesandchill/portalbackendservice/service/film/impl/FilmServiceImpl.java
@@ -1,11 +1,15 @@
 package com.moviesandchill.portalbackendservice.service.film.impl;
 
+import com.moviesandchill.portalbackendservice.dto.film.agelimit.AgeLimitDto;
+import com.moviesandchill.portalbackendservice.dto.film.country.CountryDto;
 import com.moviesandchill.portalbackendservice.dto.film.film.FilmDto;
+import com.moviesandchill.portalbackendservice.dto.film.film.FullFilmDto;
 import com.moviesandchill.portalbackendservice.dto.film.genre.GenreDto;
 import com.moviesandchill.portalbackendservice.dto.film.review.ReviewDto;
 import com.moviesandchill.portalbackendservice.dto.film.screenshot.ScreenshotDto;
 import com.moviesandchill.portalbackendservice.dto.film.staff.StaffDto;
 import com.moviesandchill.portalbackendservice.mapper.CommonMapper;
+import com.moviesandchill.portalbackendservice.mapper.FilmMapper;
 import com.moviesandchill.portalbackendservice.service.film.FilmService;
 import com.moviesandchill.portalbackendservice.utils.RestTemplateUtils;
 import lombok.extern.slf4j.Slf4j;
@@ -24,9 +28,12 @@ public class FilmServiceImpl implements FilmService {
 
     private final CommonMapper commonMapper;
 
+    private final FilmMapper filmMapper;
+
     @Autowired
-    public FilmServiceImpl(CommonMapper commonMapper) {
+    public FilmServiceImpl(CommonMapper commonMapper,FilmMapper filmMapper) {
         this.commonMapper = commonMapper;
+        this.filmMapper = filmMapper;
     }
 
 
@@ -47,6 +54,87 @@ public class FilmServiceImpl implements FilmService {
     public Optional<FilmDto> getFilmById(Long film_id) {
         String url = filmServiceUrl + "/films/" + film_id;
         return RestTemplateUtils.get(url, null, FilmDto.class);
+    }
+
+    @Override
+    public Optional<FullFilmDto> getFullFilmById(Long film_id)  {
+        Optional<FilmDto> film = getFilmById(film_id);
+        if(film.isPresent()){
+            FullFilmDto fullFilmDto = filmMapper.dtoToFullFilm(film.get());
+
+            float rating = getRatingFilmById(film_id);
+            fullFilmDto.setRating(rating);
+
+            AgeLimitDto ageLimit = getAgeLimitByFilmId(film_id).orElse(null);
+            fullFilmDto.setAgeLimit(ageLimit);
+
+            CountryDto country = getCountryByFilmId(film_id).orElse(null);
+            fullFilmDto.setCountry(country);
+
+            List<GenreDto> genres = getAllGenreWithFilm(film_id);
+            fullFilmDto.setGenres(genres);
+
+            List<StaffDto> actors = getAllActorsByFilm(film_id);
+            fullFilmDto.setActors(actors);
+
+            List<StaffDto> producers = getAllProducersByFilm(film_id);
+            fullFilmDto.setProducers(producers);
+
+            List<ReviewDto> reviews = getAllReviewWithFilm(film_id);
+            fullFilmDto.setReviews(reviews);
+
+            List<ScreenshotDto> screenshots = getAllScreenshotWithFilm(film_id);
+            fullFilmDto.setScreenshots(screenshots);
+
+            return Optional.of(fullFilmDto);
+        }
+        return null;
+    }
+
+
+
+    @Override
+    public float getRatingFilmById(Long film_id) {
+        String url = filmServiceUrl + "/films/ratingFilm/" + film_id;
+        return RestTemplateUtils.get(url, null, Float.class).orElse(0f);
+    }
+
+    @Override
+    public List<StaffDto> getAllActorsByFilm(Long film_id) {
+        String url = filmServiceUrl + "/films/" + film_id + "/actors";
+        Optional<StaffDto[]> listActorsOptional = RestTemplateUtils.get(url, StaffDto[].class);
+        return commonMapper.toList(listActorsOptional);
+    }
+
+    @Override
+    public List<StaffDto> getAllProducersByFilm(Long film_id) {
+        String url = filmServiceUrl + "/films/" + film_id + "/actors";
+        Optional<StaffDto[]> listProducersOptional = RestTemplateUtils.get(url, StaffDto[].class);
+        return commonMapper.toList(listProducersOptional);
+    }
+
+    @Override
+    public Optional<AgeLimitDto> getAgeLimitByFilmId(Long film_id) {
+        String url = filmServiceUrl + "/films/" + film_id + "/ageLimit";
+        return RestTemplateUtils.get(url, null, AgeLimitDto.class);
+    }
+
+    @Override
+    public void setAgeLimitByFilmId(Long film_id, Long ageLimitID) throws Exception {
+        String url = filmServiceUrl + "/films/" + film_id + "/setAgeLimit/" + ageLimitID;
+        RestTemplateUtils.post(url, null);
+    }
+
+    @Override
+    public Optional<CountryDto> getCountryByFilmId(Long film_id) {
+        String url = filmServiceUrl + "/films/" + film_id + "/country";
+        return RestTemplateUtils.get(url, null, CountryDto.class);
+    }
+
+    @Override
+    public void setCountryByFilmId(Long film_id, Long countryID) throws Exception {
+        String url = filmServiceUrl + "/films/" + film_id + "/setCountry/" + countryID;
+        RestTemplateUtils.post(url, null);
     }
 
     @Override
