e32c2fba664c31f3378134986cb7413b64b01c40
weldnor <kolesov.anton.s@gmail.com>
2021-05-20 12:11:39 +0300

    Add notification sending after friend request

--- src/main/java/com/moviesandchill/portalbackendservice/service/user/FriendRequestService.java
+++ src/main/java/com/moviesandchill/portalbackendservice/service/user/FriendRequestService.java
@@ -1,7 +1,9 @@
 package com.moviesandchill.portalbackendservice.service.user;
 
+import com.moviesandchill.portalbackendservice.dto.chat.notification.NewNotificationDto;
 import com.moviesandchill.portalbackendservice.dto.user.friendrequest.FriendRequestDto;
 import com.moviesandchill.portalbackendservice.dto.user.friendrequest.NewFriendRequestDto;
+import com.moviesandchill.portalbackendservice.service.chat.NotificationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -17,10 +19,12 @@ public class FriendRequestService {
 
     private final String userServiceUrl;
     private final RestTemplate restTemplate;
+    private final NotificationService notificationService;
 
-    public FriendRequestService(@Value("${endpoint.user-service.url}") String userServiceUrl, RestTemplate restTemplate) {
+    public FriendRequestService(@Value("${endpoint.user-service.url}") String userServiceUrl, RestTemplate restTemplate, NotificationService notificationService) {
         this.userServiceUrl = userServiceUrl;
         this.restTemplate = restTemplate;
+        this.notificationService = notificationService;
     }
 
     public List<FriendRequestDto> getAllFriendRequests() {
@@ -31,7 +35,11 @@ public class FriendRequestService {
 
     public FriendRequestDto addFriendRequest(NewFriendRequestDto newFriendRequestDto) {
         String url = userServiceUrl + "/api/v1/friend_requests";
-        return restTemplate.postForObject(url, newFriendRequestDto, FriendRequestDto.class);
+        long userId = newFriendRequestDto.getUserId();
+        long recipientId = newFriendRequestDto.getRecipientId();
+        var friendRequestDto = restTemplate.postForObject(url, newFriendRequestDto, FriendRequestDto.class);
+        notificationService.addNotification(new NewNotificationDto(recipientId, "user " + userId + "send friend request"));
+        return friendRequestDto;
     }
 
     public void deleteAllFriendRequests() {
